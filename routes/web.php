85d0fd8f11655e2705f844f9ef51b1551a2c2da9
HoangTrongMinhDuc <31062901+HoangTrongMinhDuc@users.noreply.github.com>
2018-11-19 09:33:46 +0700

    + add new route to dashboard and manager page

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,22 @@ Route::get('/post', function () {
 
 Route::get('/scholarship/{id}', ['as'=>'getScholarshipDetail','uses'=>'GetDataToViewController@getScholarshipDetail']);
 
+Route::get('/dashboard', function(){
+	return view('dashboard');
+});
 
+Route::get('/dashpage', 'DatabaseController@routeDashBoard');
 
-
-
+Route::group(['prefix'=>'manage'], function(){
+	Route::group(['prefix'=>'scholarship'], function(){
+		Route::get('/', 'DatabaseController@getAllScholar');
+		Route::get('delete/{id}', 'DatabaseController@delScholar');
+	});
+	Route::group(['prefix'=>'account'], function(){
+		Route::get('', 'DatabaseController@getAllAccount');
+		Route::get('ban/{id}', 'DatabaseController@banAccount');
+		Route::get('del/{id}', 'DatabaseController@delAccount');
+	});
+});
+// Route::get('/manager/{userid}', 'DatabaseController@getData');
 
